a340b4e2b86e0b0da26be6b66a58124517dba911
lollomrt <lorenzomartini.mrt@outlook.it>
2023-01-09 17:42:20 +0100

    Esercizio e bonus completi

--- js/javascript.js
+++ js/javascript.js
@@ -3,7 +3,17 @@ const { createApp } = Vue;
 createApp({
     data(){
         return{
-            
+            randomEmails: [],
+        }
+    },
+    methods:{
+        generateEmails(){
+            for(let i=0; i < 10; i++){
+                axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then((result) => {
+                this.randomEmails.push(result.data.response) 
+                })
+            }
+            console.log(this.randomEmails)
         }
     }
 }).mount("#app")
